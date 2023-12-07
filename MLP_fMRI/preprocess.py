051d8287ef9cf5508379f22ef21df99dd0209b2b
Saksham Gupta <saksham1319@gmail.com>
2023-12-08 00:57:49 +0530

    Create preprocess.py

---
+++ MLP_fMRI/preprocess.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+import sys
+import os
+from torchvision import transforms
+
+preprocess = transforms.Compose(
+    [
+        transforms.Grayscale(),  # Convert to greyscale
+        transforms.ToTensor(),
+    ]
+)
+
+
+def get_arr_from_image(path):
+    img = Image.open(path)
+    arr = preprocess(img).unsqueeze(0).cpu().detach().numpy()
+
+    # broadcast to (1, 1, 54, 45, 45)
+    arr = np.broadcast_to(arr, (1, 1, 54, 45, 45))
+
+    # reshape to (1, 1, 45, 54, 45)
+    arr = arr.reshape(1, 1, 45, 54, 45)
+
+    return arr
